4f3c325982e9259634b855d650ea34cf71c7d908
Pham Huyen Thuong <19020454@vnu.edu.vn>
2021-06-23 21:24:05 +0700

    Single Page Applications part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -35,12 +35,22 @@ class Main extends Component {
         leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return (
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        />
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/contactus" component={Contact} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} />} />
           <Redirect to="/home" />
         </Switch>

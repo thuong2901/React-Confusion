72da974431997aa0622b4cf9d5656b425f19fd66
Pham Huyen Thuong <19020454@vnu.edu.vn>
2021-06-29 22:47:51 +0700

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
     Button, Label, Col, Row, Modal, ModalHeader, ModalBody } from 'reactstrap';
     import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Link} from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length  <= len);
@@ -143,7 +143,25 @@ class CommentForm extends Component {
     }
 
     const Dishdetail = (props) => {
-        if(props.dish != null) {
+        if (props.isLoading) {
+            return (
+                <div className='container'>
+                    <div className='row'>
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return (
+                <div className='container'>
+                    <div className='row'>
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if(props.dish != null) {
             return (
                 <div class="container">
                     <div className="row">

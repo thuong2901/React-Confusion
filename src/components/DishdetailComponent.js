1b89f69f6f9858cc988697a4b4f4c3a601931be9
Pham Huyen Thuong <19020454@vnu.edu.vn>
2021-07-02 17:19:41 +0700

    fetch post

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,7 +31,7 @@ class CommentForm extends Component {
 
     handleSubmitComment(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -109,7 +109,7 @@ class CommentForm extends Component {
         );
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if(comments != null) {
             const cmts = comments.map((comment) => {
                 return (
@@ -132,7 +132,7 @@ class CommentForm extends Component {
                     <ul className="list-unstyled">
                         {cmts}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment} />
+                    <CommentForm dishId={dishId} postComment={postComment} />
                 </div>
             );
         } else {
@@ -178,7 +178,7 @@ class CommentForm extends Component {
                     <div className="row">
                         <RenderDish dish={props.dish} />
                         <RenderComments comments={props.comments} 
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id} />
                     </div>
                 </div>
